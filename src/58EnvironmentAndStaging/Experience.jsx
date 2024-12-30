5e7857aae19e3d42e5ee57d15ad2d26101c47bcb
khalili <m.khalili7310@gmail.com>
2024-12-30 13:35:48 +0330

    Accumulative shadows

--- src/58EnvironmentAndStaging/Experience.jsx
+++ src/58EnvironmentAndStaging/Experience.jsx
@@ -1,31 +1,76 @@
 import { useFrame } from "@react-three/fiber";
-import { OrbitControls, useHelper } from "@react-three/drei";
+import {
+  AccumulativeShadows,
+  BakeShadows,
+  OrbitControls,
+  RandomizedLight,
+  softShadows,
+  useHelper,
+} from "@react-three/drei";
 import { useRef } from "react";
 import { Perf } from "r3f-perf";
 import * as THREE from "three";
 
+// softShadows({
+//   frustum: 3.75,
+//   size: 50,
+//   near: 9.5,
+//   samples: 17,
+//   rings: 11,
+// });
+
 export default function Experience() {
   const cube = useRef();
   const directionalLightRef = useRef();
-  useHelper(directionalLightRef, THREE.DirectionalLightHelper, 2);
+  //   useHelper(directionalLightRef, THREE.DirectionalLightHelper, 2);
 
   useFrame((state, delta) => {
+    // const time = state.clock.elapsedTime;
+    // cube.current.position.x = 2 + Math.sin(time);
     cube.current.rotation.y += delta * 0.2;
   });
 
   return (
     <>
+      <BakeShadows />
+
       <color args={["ivory"]} attach={"background"} />
 
       <Perf position="top-left" />
 
       <OrbitControls makeDefault />
 
+      <AccumulativeShadows
+        position={[0 - 0.99, 0]}
+        scale={10}
+        color="#316d39"
+        opacity={0.8}
+        frames={Infinity}
+        temporal
+        blend={100}
+      >
+        <RandomizedLight
+          position={[1, 2, 3]}
+          amount={8}
+          radius={1}
+          ambient={0.5}
+          intensity={1}
+          bias={0.001}
+        />
+      </AccumulativeShadows>
+
       <directionalLight
         ref={directionalLightRef}
         castShadow
         position={[1, 2, 3]}
         intensity={1.5}
+        shadow-mapSize={[1024, 1024]}
+        shadow-camera-near={1}
+        shadow-camera-far={10}
+        shadow-camera-top={5}
+        shadow-camera-right={5}
+        shadow-camera-left={-5}
+        shadow-camera-bottom={-5}
       />
       <ambientLight intensity={0.5} />
 
@@ -40,7 +85,7 @@ export default function Experience() {
       </mesh>
 
       <mesh
-        receiveShadow
+        // receiveShadow
         position-y={-1}
         rotation-x={-Math.PI * 0.5}
         scale={10}
