2f13ea86a365762ff73b21875d3a2a975f782859
khalili <m.khalili7310@gmail.com>
2024-12-31 11:11:13 +0330

    Preloading

--- src/59LoadModels/Model.jsx
+++ src/59LoadModels/Model.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useLoader } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader.js";
-import { useGLTF } from "@react-three/drei";
+import { Clone, useGLTF } from "@react-three/drei";
 
 const Model = () => {
   // const model = useLoader(GLTFLoader, "./hamburger.glb");
@@ -15,7 +15,15 @@ const Model = () => {
 
   const model = useGLTF("./hamburger-draco.glb");
 
-  return <primitive object={model.scene} scale={0.35} />;
+  return (
+    <>
+      <Clone object={model.scene} scale={0.35} position-x={-4} />;
+      <Clone object={model.scene} scale={0.35} position-x={0} />;
+      <Clone object={model.scene} scale={0.35} position-x={4} />;
+    </>
+  );
 };
+
+useGLTF.preload("./hamburger-draco.glb");
 
 export default Model;
