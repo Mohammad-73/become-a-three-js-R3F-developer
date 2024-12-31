3c276b0aaa02305cbc817f8ab744e510fa1df5c5
khalili <m.khalili7310@gmail.com>
2024-12-31 11:02:38 +0330

    Loading model with drei

--- src/59LoadModels/Model.jsx
+++ src/59LoadModels/Model.jsx
@@ -2,20 +2,20 @@ import React from "react";
 import { useLoader } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader.js";
+import { useGLTF } from "@react-three/drei";
 
 const Model = () => {
   // const model = useLoader(GLTFLoader, "./hamburger.glb");
-  const model = useLoader(
-    GLTFLoader,
-    "./FlightHelmet/glTF/FlightHelmet.gltf",
-    (loader) => {
-      const dracoLoader = new DRACOLoader();
-      dracoLoader.setDecoderPath("./draco/");
-      loader.setDRACOLoader(dracoLoader);
-    }
-  );
 
-  return <primitive object={model.scene} scale={5} position-y={-1} />;
+  //   const model = useLoader(GLTFLoader, "./hamburger.glb", (loader) => {
+  //     const dracoLoader = new DRACOLoader();
+  //     dracoLoader.setDecoderPath("./draco/");
+  //     loader.setDRACOLoader(dracoLoader);
+  //   });
+
+  const model = useGLTF("./hamburger-draco.glb");
+
+  return <primitive object={model.scene} scale={0.35} />;
 };
 
 export default Model;
