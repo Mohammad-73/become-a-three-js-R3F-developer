854d28494d823d7d89652dac8ad0aac530de798e
khalili <m.khalili7310@gmail.com>
2025-01-06 10:42:31 +0330

    Creating files

---
+++ src/63PostProcessing/index.jsx
@@ -0,0 +1,20 @@
+import { Canvas } from "@react-three/fiber";
+import Experience from "./Experience.jsx";
+import "./style.css";
+
+const PostProcessing = () => {
+  return (
+    <Canvas
+      camera={{
+        fov: 45,
+        near: 0.1,
+        far: 200,
+        position: [4, 2, 6],
+      }}
+    >
+      <Experience />
+    </Canvas>
+  );
+};
+
+export default PostProcessing;
