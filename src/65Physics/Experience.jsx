a5276b62fc9a8c153c864779785524d618c4a2d6
khalili <m.khalili7310@gmail.com>
2025-01-07 11:16:50 +0330

    Jump cube function

--- src/65Physics/Experience.jsx
+++ src/65Physics/Experience.jsx
@@ -1,8 +1,20 @@
 import { OrbitControls } from "@react-three/drei";
 import { Debug, Physics, RigidBody } from "@react-three/rapier";
 import { Perf } from "r3f-perf";
+import { useRef } from "react";
 
 export default function Experience() {
+  const cubeRef = useRef();
+
+  const cubeJump = () => {
+    cubeRef.current.applyImpulse({ x: 0, y: 5, z: 0 });
+    cubeRef.current.applyTorqueImpulse({
+      x: Math.random() - 0.5,
+      y: Math.random() - 0.5,
+      z: Math.random() - 0.5,
+    });
+  };
+
   return (
     <>
       <Perf position="top-left" />
@@ -14,17 +26,31 @@ export default function Experience() {
 
       <Physics>
         <Debug />
-        <RigidBody>
-          <mesh castShadow position={[-2, 2, 0]}>
+        <RigidBody colliders="ball">
+          <mesh castShadow position={[-1.5, 2, 0]}>
             <sphereGeometry />
             <meshStandardMaterial color="orange" />
           </mesh>
         </RigidBody>
 
-        <mesh castShadow position={[2, 2, 0]}>
-          <boxGeometry />
-          <meshStandardMaterial color="mediumpurple" />
-        </mesh>
+        <RigidBody ref={cubeRef} position={[1.5, 2, 0]}>
+          <mesh onClick={cubeJump} castShadow>
+            <boxGeometry />
+            <meshStandardMaterial color="mediumpurple" />
+          </mesh>
+        </RigidBody>
+
+        {/* <RigidBody
+          colliders={false}
+          position={[0, 1, 0]}
+          rotation={[Math.PI * 0.5, 0, 0]}
+        >
+          <CuboidCollider args={[1.5, 1.5, 1.5]} />
+          <mesh castShadow>
+            <torusGeometry args={[1, 0.5, 16, 32]} />
+            <meshStandardMaterial color="mediumpurple" />
+          </mesh>
+        </RigidBody> */}
 
         <RigidBody type="fixed">
           <mesh receiveShadow position-y={-1.25}>
