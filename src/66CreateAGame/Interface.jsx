26253fa438d9567c9d5fac94b51adda9000341ac
khalili <m.khalili7310@gmail.com>
2025-01-08 16:14:32 +0330

    Time interface

---
+++ src/66CreateAGame/Interface.jsx
@@ -0,0 +1,10 @@
+const Interface = () => {
+  return (
+    <div className="interface">
+      {/* Time */}
+      <div className="time">0.00</div>
+    </div>
+  );
+};
+
+export default Interface;
